572dc5adf231d917ba15941dbaa82b9175e57e65
warnec <warnec@oregonstate.edu>
2021-01-30 20:32:43 -0800

    Added user input checking functionality to program

--- LeapYear.py
+++ LeapYear.py
@@ -2,20 +2,24 @@
 #Program to check whether the inputted year is a leap year
 
 
-# get a year from the user
-print("Please enter a year to check whether it is a leap year")
-yearInput = int(input())
+while(1):
+    # get a year from the user
+    print("Please enter a year to check whether it is a leap year")
+    try:
+        yearInput = int(input())
+        if ((yearInput) % 4) == 0:
+            if ((yearInput) % 100) == 0:
+                if ((yearInput) % 400) == 0:
+                    print("The entered year is a leap year")
+                else:
+                    print("The entered year is not a leap year")
+            else:
+                print("The entered year is a leap year")
+        else:
+            print("The entered year is not a leap year")
+    except ValueError:
+        print("You must enter an integer with no spaces!")
 
-#check if the year is divisible by the given numbers in sequential order.  The if statement checking for whether
-#the year is divisible by 100 is a special case and will output that the year is not a leap year unless the innermost
-#if statment clears as well
-if ((yearInput) % 4) == 0:
-   if ((yearInput) % 100) == 0:
-       if ((yearInput) % 400) == 0:
-           print("The entered year is a leap year")
-       else:
-           print("The entered year is not a leap year")
-   else:
-       print("The entered year is a leap year")
-else:
-   print("The entered year is not a leap year")
+    # check if the year is divisible by the given numbers in sequential order.  The if statement checking for whether
+    # the year is divisible by 100 is a special case and will output that the year is not a leap year unless the innermost
+    # if statment clears as well
